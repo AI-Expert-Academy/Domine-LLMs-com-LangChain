c4151a0af1f2be4f9468245bf8859cfef67e2923
João Paulo Maida <jpmaida@gmail.com>
2025-06-14 21:13:27 -0300

    Configuração da chain completa

--- main.py
+++ main.py
@@ -103,3 +103,58 @@ def config_retriever(uploads):
                                          search_kwargs={"k": 3, 'fetch_k': 4})
 
     return retriever
+
+# Configuração da chain completa
+def config_rag_chain(model_class, retriever):
+    print(retriever)
+    
+    # Carregamento da LLM
+    if model_class == "hf_hub":
+        llm = model_hf_hub()
+    elif model_class == "openai":
+        llm = model_openai()
+    elif model_class == "ollama":
+        llm = model_ollama()
+    elif model_class == "groq":
+        llm = model_groq()
+    else:
+        raise ValueError("Modelo não suportado")
+
+    if model_class.startswith("hf"):
+        token_s, token_e = "<|begin_of_text|><start_header_id|>system<|end_header_id|>", "<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+        # token_s, token_e = "<|system|>", "<|end|><|assistant|>" # phi3
+    else:
+        token_s, token_e = "", ""
+    
+    context_q_system_prompt = "Given the following chat history and the follow-up question which might reference context in the chat history, formulate a standalone question which can be understood without the chat history. Do NOT answer the question, just reformulate it if needed and otherwise return it as is."
+    context_q_system_prompt = token_s + context_q_system_prompt
+    context_q_user_prompt = "Question: {input}" + token_e
+    context_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", context_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", context_q_user_prompt)
+        ]
+    )
+
+    # Chain para contextualização
+    history_aware_retriever = create_history_aware_retriever(llm=llm,
+                                                             retriever=retriever,
+                                                             prompt=context_q_prompt)
+    
+    # Prompt para perguntas e respostas (Q&A)
+    qa_prompt_template = """
+    Você é um assistente virtual prestativo e está respondendo perguntas gerais.  
+    Use os seguintes pedações de contexto recuperado para responder à pergunta. 
+    Se você não sabe a resposta, apenas diga que não sabe. Mantenha a resposta concisa. 
+    Responda em português. \n\n
+    Pergunta: {input} \n
+    Contexto: {context}"""
+
+    qa_prompt = PromptTemplate.from_template(token_s + qa_prompt_template + token_e)
+
+    # Configurar LLM e chain de perguntas e respostas (Q&A)
+    qa_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, qa_chain,)
+
+    return rag_chain
